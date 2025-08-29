04a37cbdd0912b4f9b75bc80498df6e448f1744e
dbdnsdb123 <dbdnsdb123@naver.com>
2025-08-29 11:58:41 +0900

    레벨 2-3: ref User changePassword logic

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserChangePasswordRequest {
+    private static final String PASSWORD_REGEX =
+            "^(?=.*[0-9])(?=.*[A-Z]).{8,}$";
 
     @NotBlank
+    @Pattern(regexp = PASSWORD_REGEX)
     private String oldPassword;
+
     @NotBlank
+    @Pattern(regexp = PASSWORD_REGEX)
     private String newPassword;
 }
