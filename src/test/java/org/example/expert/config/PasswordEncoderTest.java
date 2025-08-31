a9e6935f870447a2dd9aa72a81c86b759647f7a3
dbdnsdb123 <dbdnsdb123@naver.com>
2025-08-31 20:52:20 +0900

    레벨 4-1: ref PasswordEncoderTest

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -1,16 +1,15 @@
 package org.example.expert.config;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class PasswordEncoderTest {
 
-    @InjectMocks
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Test
@@ -20,7 +19,7 @@ class PasswordEncoderTest {
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
